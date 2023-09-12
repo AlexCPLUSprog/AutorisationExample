23661948ffa03e60aff353086ef65e9806c4a8b7
Алекс <Alexandr_enid@mail.ru>
2023-09-12 18:57:28 +0500

    Добавьте файлы проекта.

---
+++ AutorisationExample/Form1.cs
@@ -0,0 +1,124 @@
+﻿using NetworkUser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutorisationExample
+{
+    public partial class Form1 : Form
+    {
+        private List<User> users;
+        //
+        private string dbName = "user_database.db";
+        private string create_table = $"CREATE TABLE IF NOT EXISTS REQUISITES (" +
+            $"ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+            $"NAME NVARCHAR(25), PASSWORD NVARCHAR(25), " +
+            $"ROLE NVARCHAR(15));";
+        private string init_data = $"INSERT INTO REQUISITES (" +
+            $"NAME, PASSWORD, ROLE" +
+            $") VALUES  (" +
+            $"'Admin', '0', 'Пользователь');";
+        private string show_all_data = $"SELECT * FROM REQUISITES;";
+        private string delete_main_table = $"DROP TABLE IF EXISTS REQUISITES;";
+        private SQLiteDataAdapter _adapter;
+        private DataSet _dataSet;
+        private DBWork dBWork;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            users = new List<User>();
+            initDB();
+        }
+        private void initDB()
+        {
+            Action<string> action = (string _text) => { lbConnect.Text = _text; };
+            dBWork = new DBWork(dbName, action, create_table);
+            doSqlQuery(init_data);
+            _adapter = dBWork.getDataAdapter;
+            _dataSet = dBWork.GetDataSet(show_all_data, action);
+            // Создаём привязку
+            BindingSource bindingSource1 = new BindingSource();
+            bindingSource1.DataSource = dBWork.getDataSet.Tables[0];
+            dataGridViewUsers.DataSource = bindingSource1;
+            SQLiteCommandBuilder sQLiteCommandBuilder = new SQLiteCommandBuilder(_adapter);
+            _adapter.Update(_dataSet);
+        }
+
+        private void updateDB()
+        {
+            _adapter = dBWork.getDataAdapter;
+            Action<string> action = (string _text) => { lbConnect.Text = _text; };
+            _dataSet = dBWork.GetDataSet("default", action);
+            // Создаём привязку 
+            BindingSource bindingSource1 = new BindingSource();
+            bindingSource1.DataSource = dBWork.getDataSet.Tables[0];
+            dataGridViewUsers.DataSource = bindingSource1;
+            SQLiteCommandBuilder sQLiteCommandBuilder = new SQLiteCommandBuilder(_adapter);
+            _adapter.Update(_dataSet);
+        }
+        private void doSqlQuery(string query)
+        {
+            Action<string> action = (string _text) => { lbConnect.Text = _text; };
+            dataGridViewUsers.DataSource = dBWork.GetDataSet(query, action);
+        }
+        private void addUserToDB(string name, int password)
+        {
+            string add_data = $"INSERT INTO REQUISITES (" +
+            $"NAME, PASSWORD, ROLE" +
+            $") VALUES  (" +
+            $"'{name}', '{password}', 'Пользователь');";
+            doSqlQuery(add_data);
+        }
+        private void btnAddUser_Click(object sender, EventArgs e)
+        {
+            if (textBoxLogin.Text != string.Empty && maskedTextBoxPassword.Text != string.Empty)
+            {
+                User user = new User(
+                    textBoxLogin.Text,
+                    maskedTextBoxPassword.Text.GetHashCode());
+                users.Add(user);
+                labelUsers.Text = "Активные пользователи:\r\n";
+                foreach (var item in users)
+                {
+                    addUserToDB(item.Name, item.Password);
+                    updateDB();
+                    labelUsers.Text += $"{item.Name} {item.Password}" + "\r\n";
+                }
+            }
+        }
+
+        private void buttonTestLogin_Click(object sender, EventArgs e)
+        {
+            var login = textBoxLogin.Text;
+            var password = maskedTextBoxPassword.Text.GetHashCode();
+            User user = new User(login, password);
+            
+            foreach (var item in users)
+            {
+                if (user.Name == item.Name && user.Password == item.Password)
+                {
+                    MessageBox.Show($"Пользователь {login} существует");
+                }
+            }
+            
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            updateDB();
+        }
+
+        private void buttonSaveToDB_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ AutorisationExample/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutorisationExample
+{
+    enum Roles
+    {
+        Administrator,
+        User,
+        Master
+    }
+    internal class User
+    {
+        private string name;
+        private int password;
+        private Roles role;
+        public string Name { get { return name; } }
+        public int Password { get { return password; } }
+        public Roles Role { get { return role; } set { role = value; } }
+        public User(string name, int password, Roles role = Roles.User)
+        {
+            this.name = name;
+            this.password = password;
+            this.role = role;
+        }
+        
+    }
+}
